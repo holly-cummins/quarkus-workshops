7e7caa385435e7872b0312429fc9b36cb8478af4
Holly Cummins <hcummins@redhat.com>
2022-10-07 12:02:49 +0200

    Fix break caused by long image name

--- quarkus-workshop-super-heroes/super-heroes/rest-villains/src/main/resources/import.sql
+++ quarkus-workshop-super-heroes/super-heroes/rest-villains/src/main/resources/import.sql
@@ -2,22 +2,22 @@
 INSERT INTO villain(id, name, otherName, picture, powers, level)
 VALUES (nextval('hibernate_sequence'), 'Buuccolo', 'Majin Buu',
         'https://www.superherodb.com/pictures2/portraits/11/050/15355.jpg',
-        'Accelerated Healing, Adaptation, Agility, Flight, Immortality, Intelligence, Invulnerability, Reflexes, Self-Sustenance, Size Changing, Spatial Awareness, Stamina, Stealth, Super Breath, Super Speed, Super Strength, Teleportation',
+        'Accelerated Healing, Adaptation, Agility, Flight, Immortality, Intelligence, Invulnerability, Reflexes, Self-Sustenance',
         22);
 INSERT INTO villain(id, name, otherName, picture, powers, level)
 VALUES (nextval('hibernate_sequence'), 'Drømmekage', 'Majin Buu',
-        'https://www.google.com/imgres?imgurl=https%3A%2F%2Fworld.openfoodfacts.org%2Fimages%2Fproducts%2F308%2F021%2F605%2F5084%2Ffront_fr.31.full.jpg&imgrefurl=https%3A%2F%2Fworld.openfoodfacts.org%2Fproduct%2F3080216055084%2Fcarlsberg-6x33cl-carlsberg-5-0-degre-alcool&tbnid=a0gomdfYGfOM-M&vet=12ahUKEwiV3PLDz8z6AhVQmYsKHXjKD-QQMygBegQIARBy..i&docid=BjRUqy4Yn_zieM&w=2344&h=2136&q=carlsberg&hl=en&client=firefox-b-e&ved=2ahUKEwiV3PLDz8z6AhVQmYsKHXjKD-QQMygBegQIARBy',
-        'Accelerated Healing, Adaptation, Agility, Flight, Immortality, Intelligence, Invulnerability, Reflexes, Self-Sustenance, Size Changing, Spatial Awareness, Stamina, Stealth, Super Breath, Super Speed, Super Strength, Teleportation',
+        'https://dk.openfoodfacts.org/images/products/570/102/900/1720/front_en.3.full.jpg',
+        'Accelerated Healing, Adaptation, Agility, Flight, Immortality, Intelligence, Invulnerability, Reflexes, Self-Sustenance',
         22);
 INSERT INTO villain(id, name, otherName, picture, powers, level)
 VALUES (nextval('hibernate_sequence'), 'Kransekake', 'Majin Buu',
         'https://images.notquitenigella.com/images/kransekake-a-danish-wedding-cake/__m-kransekake2.jpg',
-        'Accelerated Healing, Adaptation, Agility, Flight, Immortality, Intelligence, Invulnerability, Reflexes, Self-Sustenance, Size Changing, Spatial Awareness, Stamina, Stealth, Super Breath, Super Speed, Super Strength, Teleportation',
+        'Accelerated Healing, Adaptation, Agility, Flight, Immortality, Intelligence, Invulnerability, Reflexes, Self-Sustenance',
         22);
 INSERT INTO villain(id, name, otherName, picture, powers, level)
 VALUES (nextval('hibernate_sequence'), 'Frøsnapper', 'Majin Buu',
         'https://upload.wikimedia.org/wikipedia/commons/f/fd/Fr%C3%B8snapper_tebirkes_grovbirkes_%28cropped_to_tebirkes%29.jpg',
-        'Accelerated Healing, Adaptation, Agility, Flight, Immortality, Intelligence, Invulnerability, Reflexes, Self-Sustenance, Size Changing, Spatial Awareness, Stamina, Stealth, Super Breath, Super Speed, Super Strength, Teleportation',
+        'Accelerated Healing, Adaptation, Agility, Flight, Immortality, Intelligence, Invulnerability, Reflexes, Self-Sustenance',
         27);
 INSERT INTO villain(id, name, otherName, picture, powers, level)
 VALUES (nextval('hibernate_sequence'), 'Hindbærsnitter', 'Majin Buu',
